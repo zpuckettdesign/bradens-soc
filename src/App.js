d62f445436297754d94dc7c5de911f09ec94e6ae
Zach Puckett <zpuckettdesign@gmail.com>
2021-09-22 11:20:45 -0500

    refactor all files

--- src/App.js
+++ src/App.js
@@ -4,30 +4,6 @@ import Header from "./Header";
 import Converter from "./Converter";
 import vendors from './Vendors'
 
-// export default function App() {
-//   const [value, setValue] = useState([]);
-
-//   return (
-//     <>
-//       <div className="header">
-//         <Header />
-//       </div>
-//       <div className="converter">
-//         <Converter
-//           prompt="Select Vendor..."
-//           options={vendors}
-//           id="name"
-//           markup="markup"
-//           value={value}
-//           onChange={(val) => setValue(val)}
-//         />
-//       </div>
-//       <div>
-//         <Price />
-//       </div>
-//     </>
-//   );
-// }
 
 export default class App extends Component {
   render() {
