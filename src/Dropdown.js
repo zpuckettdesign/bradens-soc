af8bf643b76e110f853e8dcb26ecce89ee50c56a
zpuckettdesign <zpuckettdesign@gmail.com>
2021-09-24 11:15:42 -0500

    add new dropdown

---
+++ src/Dropdown.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+
+export default function ComboBox() {
+  return (
+    <Autocomplete
+      disablePortal
+      id="combo-box"
+      options={vendor}
+      sx={{ width: 300 }}
+      renderInput={(params) => <TextField {...params} label="Vendor" />}
+    />
+  );
+}
